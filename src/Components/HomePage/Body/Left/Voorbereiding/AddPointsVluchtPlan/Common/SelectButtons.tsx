fd6563c48335e8f618f7464944e1cbf8c5334d68
Tab Geo Hub <tabgeohub@gmail.com>
2026-02-24 02:01:43 +0200

    Add geoemetries to select all

--- src/Components/HomePage/Body/Left/Voorbereiding/AddPointsVluchtPlan/Common/SelectButtons.tsx
+++ src/Components/HomePage/Body/Left/Voorbereiding/AddPointsVluchtPlan/Common/SelectButtons.tsx
@@ -1,26 +1,51 @@
 import { useAddPointStates } from "../../../../../../../hooks/zustand/useAddPointStates";
 import { usePointsStore } from "hooks/features/usePointsStore";
+import { useGeometriesStore } from "hooks/features/useGeometriesStore";
 
 export default function SelectButtons({ herhalen }: { herhalen: boolean }) {
   const { points } = usePointsStore();
+  const { geometries } = useGeometriesStore();
 
   const { setSelectedPoints, setSelectedPoints2 } = useAddPointStates();
 
   function handleSelectAll() {
     if (herhalen === false) {
       const herhalenPoints = points.filter((point) => point.herhalen === 0);
+      const herhalenGeometries = geometries.filter((geometry) => {
+        const herhalenValue =
+          typeof geometry.herhalen === "number"
+            ? geometry.herhalen === 0
+            : typeof geometry.herhalen === "string"
+              ? geometry.herhalen === "0"
+              : geometry.herhalen === false;
+        return herhalenValue;
+      });
 
       setSelectedPoints2(herhalenPoints.flatMap((point) => point.id));
+      // Note: Geometry selection will be added when geometries are integrated into AddPointsVluchtPlan
+      // For now, geometries are filtered but not selected since useAddPointStates doesn't have geometry state yet
     } else {
       const herhalenPoints = points.filter((point) => point.herhalen === 1);
+      const herhalenGeometries = geometries.filter((geometry) => {
+        const herhalenValue =
+          typeof geometry.herhalen === "number"
+            ? geometry.herhalen === 1
+            : typeof geometry.herhalen === "string"
+              ? geometry.herhalen === "1"
+              : geometry.herhalen === true;
+        return herhalenValue;
+      });
 
       setSelectedPoints(herhalenPoints.flatMap((point) => point.id));
+      // Note: Geometry selection will be added when geometries are integrated into AddPointsVluchtPlan
+      // For now, geometries are filtered but not selected since useAddPointStates doesn't have geometry state yet
     }
   }
 
   function handleSelectNone() {
     setSelectedPoints([]);
     setSelectedPoints2([]);
+    // Note: Geometry deselection will be added when geometries are integrated into AddPointsVluchtPlan
   }
 
   return (
--- src/Components/HomePage/Body/Left/Voorbereiding/FlightPlan/Common/SelectButtons.tsx
+++ src/Components/HomePage/Body/Left/Voorbereiding/FlightPlan/Common/SelectButtons.tsx
@@ -1,26 +1,55 @@
 import { useContent } from "hooks/useContent";
 import { useFlightPlanState } from "../helpers/flightPlanStates";
 import { usePointsStore } from "hooks/features/usePointsStore";
+import { useGeometriesStore } from "hooks/features/useGeometriesStore";
 
 export default function SelectButtons({ herhalen }: { herhalen: boolean }) {
-  const { setSelectedPoints, setSelectedPoints2 } = useFlightPlanState();
+  const { 
+    setSelectedPoints, 
+    setSelectedPoints2,
+    setSelectedGeometries,
+    setSelectedGeometries2 
+  } = useFlightPlanState();
   const { points } = usePointsStore();
+  const { geometries } = useGeometriesStore();
 
   function handleSelectAll() {
     if (herhalen === false) {
       const notHerhalenPoints = points.filter((point) => point.herhalen === 0);
+      const notHerhalenGeometries = geometries.filter((geometry) => {
+        const herhalenValue =
+          typeof geometry.herhalen === "number"
+            ? geometry.herhalen === 0
+            : typeof geometry.herhalen === "string"
+              ? geometry.herhalen === "0"
+              : geometry.herhalen === false;
+        return herhalenValue;
+      });
 
       setSelectedPoints2(notHerhalenPoints.flatMap((point) => point.id));
+      setSelectedGeometries2(notHerhalenGeometries.flatMap((geometry) => geometry.id));
     } else {
       const herhalenPoints = points.filter((point) => point.herhalen === 1);
+      const herhalenGeometries = geometries.filter((geometry) => {
+        const herhalenValue =
+          typeof geometry.herhalen === "number"
+            ? geometry.herhalen === 1
+            : typeof geometry.herhalen === "string"
+              ? geometry.herhalen === "1"
+              : geometry.herhalen === true;
+        return herhalenValue;
+      });
 
       setSelectedPoints(herhalenPoints.flatMap((point) => point.id));
+      setSelectedGeometries(herhalenGeometries.flatMap((geometry) => geometry.id));
     }
   }
 
   function handleSelectNone() {
     setSelectedPoints([]);
     setSelectedPoints2([]);
+    setSelectedGeometries([]);
+    setSelectedGeometries2([]);
   }
 
   const content = useContent();
